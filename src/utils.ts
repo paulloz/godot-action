34fa78a6d3e448a5402ed837d52c0ceaa9101ebb
Turner Bass <turner@astropanda.studio>
2022-10-07 05:48:59 -0500

    Update utils.ts to support 3.5.1 stable as a valid version (#54)
    Co-authored-by: Paul Joannon <hello@pauljoannon.com>

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ const validVersions: string[] = [
     '3.2', '3.2.1', '3.2.2', '3.2.3', '3.2.4',
     '3.3', '3.3.1', '3.3.2', '3.3.3', '3.3.4',
     '3.4', '3.4.1', '3.4.2', '3.4.3', '3.4.4', '3.4.5',
-    '3.5',
+    '3.5', '3.5.1'
 ]
 
 export function validateVersion(version: string): boolean {
